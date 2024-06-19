9e89903f29b75c02d461caa3a5526872849fcc6a
Nikolai <nikolay@perevedite.im>
2024-06-19 20:38:22 +0300

    rrrrr

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,77 @@
+import duckdb
 import streamlit as st
-from pathlib import Path
-
-import pandas as pd
-import numpy as np
-import gdown
+from generate_data import generate_dataset_orders, load_file
 
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-    
-    url = "https://drive.google.com/uc?id=1t_7rVTR7iLV1aYOf4WiT66jgfPMSnLuy"
-    output = "data/diaries_premier.csv"
-    gdown.download(url, output, quiet=True)
-
-    path = Path(__file__).parent/'data/diaries_premier.csv'
-    ddf = pd.read_csv(path, index_col="id")
-
-    return ddf
+db = "duck.db"
+destination_table_name = "orders"
+filename = "orders.csv"
 
 
+def execute_query(query: str, db: str, return_type: str = "df"):
+    with duckdb.connect(db, read_only=True) as con:
+        if return_type == "df":
+            return con.execute(query).df()
+        elif return_type == "arrow":
+            return con.execute(query).arrow()
+        elif return_type == "list":
+            return con.execute(query).fetchall()
 
-ddf = get_gdp_data()
 
-st.dataframe(ddf, use_container_width=True)
+@st.cache_data  # An optimization wrapper to memoize the result of the function
+def export_df(df):
+    return df.to_csv(index=False).encode("utf-8")
+
+
+if __name__ == "__main__":
+    st.title("Streamlit + duckdb")
+    try:
+        button = st.button(label="Generate or Refresh Data")
+        if button:
+            generate_dataset_orders(filename=filename, num_rows=1000)
+            load_file(db=db, infile_path=filename, table_name=destination_table_name)
+
+        data = execute_query(f"select * from {destination_table_name}", db=db, return_type="df")
+
+        st.write("## Sample")
+        st.dataframe(data.head(10), height=300)
+
+        st.write("## Visualization")
+        option = st.selectbox("Select a dimension", ["product_name", "customer_name", "status"], key="option")
+        if option:
+            st.write(f"### Bar Chart: {option} x Quantity")
+            st.bar_chart(data, x=option, y="quantity")
+
+            st.write(f"### Bar Chart: {option} x Amount")
+            st.bar_chart(data, x=option, y="amount")
+
+            st.write(f"### Bar Chart: {option} x Count")
+            st.bar_chart(data[option].value_counts())
+
+        st.write("## Filters (by Products Name)")
+        products_list = [
+            row[0]
+            for row in execute_query(
+                f"select distinct(product_name) from {destination_table_name}", db=db, return_type="list"
+            )
+        ]
+        product_filter = st.selectbox(label="Select a Product", options=products_list, key="product_filter")
+        if product_filter != "--":
+            result = execute_query(
+                f"select * from {destination_table_name} where product_name = '{product_filter}'",
+                db=db,
+                return_type="df",
+            )
+            st.dataframe(result, height=400)
+
+            # To download the data we have just selected
+            st.write("### Download Data")
+            st.download_button(
+                label="Press to Download",
+                data=export_df(result),
+                file_name=f"orders - product='{product_filter}'.csv",
+                mime="text/csv",
+                key="download-csv",
+            )
+    except duckdb.CatalogException:  # Catch exception when the database file don't exist yet
+        st.text("Please Clik on the above button to generate data.")
---
+++ streamlit_app_rename.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+import gdown
+
+
+@st.cache_data
+def get_gdp_data():
+    """Grab GDP data from a CSV file.
+
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
+    
+    url = "https://drive.google.com/uc?id=1t_7rVTR7iLV1aYOf4WiT66jgfPMSnLuy"
+    output = "data/diaries_premier.csv"
+    gdown.download(url, output, quiet=True)
+
+    path = Path(__file__).parent/'data/diaries_premier.csv'
+    ddf = pd.read_csv(path, index_col="id")
+
+    return ddf
+
+
+
+ddf = get_gdp_data()
+
+st.dataframe(ddf, use_container_width=True)
